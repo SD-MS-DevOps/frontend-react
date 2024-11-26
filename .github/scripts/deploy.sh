61a41b528af47a8183c299e4d85af24af08f73cc
marcepeyo <marcepeyo@gmail.com>
2024-11-25 23:26:56 -0300

    se cambia archivo deploy.sh

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -4,23 +4,29 @@ set -e
 # Cambiar al directorio de salida
 cd out
 
-# Sincronizar favicons con fuerte caché
-aws s3 sync ./common/favicons s3://$S3_ORIGIN_BUCKET/common/favicons \
-  --exclude "site.webmanifest" \
-  --metadata-directive 'REPLACE' \
-  --cache-control max-age=31536000,public,must-revalidate \
-  --delete
+# Sincronizar favicon.ico con fuerte caché
+if [ -f "./apps/catalog/src/favicon.ico" ]; then
+  aws s3 cp ./apps/catalog/src/favicon.ico s3://$S3_ORIGIN_BUCKET/common/favicons/favicon.ico \
+    --metadata-directive 'REPLACE' \
+    --cache-control max-age=31536000,public,must-revalidate
+else
+  echo "El archivo favicon.ico no se encontró. Omitiendo sincronización."
+fi
 
 # Sincronizar assets con fuerte caché
-aws s3 sync ./common/assets s3://$S3_ORIGIN_BUCKET/common/assets \
-  --metadata-directive 'REPLACE' \
-  --cache-control max-age=31536000,public,must-revalidate \
-  --delete
+if [ -d "./apps/catalog/src/assets" ]; then
+  aws s3 sync ./apps/catalog/src/assets s3://$S3_ORIGIN_BUCKET/common/assets \
+    --metadata-directive 'REPLACE' \
+    --cache-control max-age=31536000,public,must-revalidate \
+    --delete
+else
+  echo "La carpeta ./apps/catalog/src/assets no se encontró. Omitiendo sincronización."
+fi
 
 # Sincronizar otros archivos generados por NX
 aws s3 sync ./ s3://$S3_ORIGIN_BUCKET \
-  --exclude "common/favicons/*" \
-  --exclude "common/assets/*" \
+  --exclude "apps/catalog/src/assets/*" \
+  --exclude "apps/catalog/src/favicon.ico" \
   --metadata-directive 'REPLACE' \
   --cache-control no-cache,no-store,must-revalidate \
   --delete
